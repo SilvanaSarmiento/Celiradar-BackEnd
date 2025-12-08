454b3f966412197e49296fc0ad253d394c702454
SilvanaSarmiento <sil22sarmiento@gmail.com>
2025-12-08 16:14:32 -0300

    configuraciones de servidor BD

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+  origin:["https://celiradar-backend.onrender.com", 
+          "http://localhost:3001" ],
+          METHODS: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+          credentials:'false'
+
+  });
+
   const config = app.get(ConfigService);
-  const frontend = config.get<string>('FRONTEND_URL') || 'http://localhost:5173';
+  const frontend = config.get<string>('https://celiradar-backend.onrender.com') || 'http://localhost:5173';
   app.enableCors({
     origin: frontend,
     credentials: true,
