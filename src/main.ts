4d06fb397383459ae9fd0cd8a6810017c72ff2e3
SilvanaSarmiento <sil22sarmiento@gmail.com>
2025-12-09 19:46:16 -0300

    Fix CORS en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin:["https://celiradar-backend.onrender.com", 
-          "http://localhost:3001" ],
-          METHODS: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-          credentials:'false'
-
+    origin: [
+      "https://celiradar.web.app",        
+      "http://localhost:3000"            
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization, Accept",
+    credentials: false 
   });
 
- await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
 
